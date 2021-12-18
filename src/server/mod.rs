aa7ed615cc450e4bc4372c69aa849d75b60efc8e
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2021-12-18 13:26:58 -0500

    Moved a log statement to the correct location

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -28,9 +28,9 @@ pub trait ServerApplication<C: Server<C, A>, A: ServerApplication<C, A>> {
 }
 
 pub async fn initialize_server_loop(server_type: &ServerType, http_client: &Client) {
-    loop {
-        println!("{} Starting server initialization...", emoji::symbols::alphanum::INFORMATION.glyph);
+    println!("{} Starting server initialization...", emoji::symbols::alphanum::INFORMATION.glyph);
 
+    loop {
         if let Err(e) = run_configured_server(server_type, http_client).await {
             println!("{} Server encountered unrecoverable error: {}", emoji::symbols::other_symbol::CROSS_MARK.glyph, e);
             break;
