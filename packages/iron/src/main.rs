44fbd8f31af284cebd12cca5cf5ce5c019520366
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2023-05-30 19:30:43 -0500

    Adding more project files

--- packages/iron/src/main.rs
+++ packages/iron/src/main.rs
@@ -12,5 +12,5 @@ type Result<T> = anyhow::Result<T, IronError>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    start_server("0.0.0.0:8080")?.await
+    start_server("localhost:8080")?.await
 }
