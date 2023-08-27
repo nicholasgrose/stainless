7a36265dfec97ea60f9f6ad3da7e83cce715af95
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2023-08-27 13:32:51 -0500

    Implement saving of PaperMC server definitions
    This commit adds the ability to save PaperMC server definitions in the database. A new function, save_paper_mc_server, was introduced in the database module to handle this task. It inserts a new record for each of Application, MinecraftServer, and PaperMcServer entities. It also updates the create_paper_mc_server method in the IronMinecraftServerCreator struct of the web module to call the new method and return a newly created server id. The ServerDefinition message in the server.proto file was slightly modified - changing 'Name' field to 'name'.

--- packages/iron/server/src/database/mod.rs
+++ packages/iron/server/src/database/mod.rs
@@ -1,1 +1,59 @@
+use anyhow::Context;
+use sea_orm::ActiveValue::Set;
+use sea_orm::{ActiveModelTrait, DatabaseConnection, TransactionTrait};
+use uuid::Uuid;
 
+use entity::application::ActiveModel as Application;
+use entity::minecraft_server::ActiveModel as MinecraftServer;
+use entity::paper_mc_server::ActiveModel as PaperMcServer;
+use iron_api::minecraft_service::{PaperMcProject, PaperMcServerDefinition};
+
+pub async fn save_paper_mc_server(
+    db: &DatabaseConnection,
+    papermc_definition: &PaperMcServerDefinition,
+) -> anyhow::Result<String> {
+    let minecraft_server_definition = papermc_definition
+        .minecraft_server_definition
+        .as_ref()
+        .with_context(|| "no minecraft server definition provided")?;
+    let server_definition = minecraft_server_definition
+        .server_definition
+        .as_ref()
+        .with_context(|| "no server definition provided")?;
+
+    let paper_mc_project = PaperMcProject::from_i32(papermc_definition.project)
+        .with_context(|| "invalid paper mc project provided")?;
+
+    let new_server_id = Uuid::new_v4();
+    let id_string = new_server_id.to_string();
+
+    let transaction = db.begin().await?;
+
+    Application {
+        id: Set(id_string.clone()),
+        name: Set(server_definition.name.clone()),
+        command: Set("".to_string()),
+    }
+    .insert(&transaction)
+    .await?;
+
+    MinecraftServer {
+        id: Set(id_string.clone()),
+        game_version: Set(minecraft_server_definition.game_version.clone()),
+    }
+    .insert(&transaction)
+    .await?;
+
+    PaperMcServer {
+        id: Set(id_string.clone()),
+        project: Set(paper_mc_project.as_str_name().to_string()),
+        build: Set(0),
+        build_update_off: Set(false),
+    }
+    .insert(&transaction)
+    .await?;
+
+    transaction.commit().await?;
+
+    Ok(new_server_id.to_string())
+}
--- packages/iron/server/src/web/grpc/mod.rs
+++ packages/iron/server/src/web/grpc/mod.rs
@@ -6,6 +6,8 @@ use iron_api::minecraft_service::minecraft_server_creator_server::MinecraftServe
 use iron_api::minecraft_service::PaperMcServerDefinition;
 use iron_api::ServerCreateResponse;
 
+use crate::database::save_paper_mc_server;
+
 #[derive(Debug, Default)]
 pub struct IronMinecraftServerCreator {
     pub db_connection: DatabaseConnection,
@@ -16,13 +18,15 @@ impl MinecraftServerCreator for IronMinecraftServerCreator {
     #[instrument]
     async fn create_paper_mc_server(
         &self,
-        _: Request<PaperMcServerDefinition>,
+        request: Request<PaperMcServerDefinition>,
     ) -> tonic::Result<Response<ServerCreateResponse>> {
         tracing::info!("creating papermc server");
 
-        let response = Response::new(ServerCreateResponse {
-            id: uuid::Uuid::new_v4().to_string(),
-        });
+        let id = save_paper_mc_server(&self.db_connection, request.get_ref())
+            .await
+            .map_err(|err| tonic::Status::from_error(err.into()))?;
+
+        let response = Response::new(ServerCreateResponse { id });
 
         tracing::info!("{:?}", response);
 
