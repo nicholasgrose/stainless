89ab1de35dd0abeaf57fa3d3b2691bf62622c0a4
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2023-09-24 12:14:38 -0500

    Refactored the app control to better isolate the app task spawning functionality

--- packages/iron/server/src/manager/app/control.rs
+++ packages/iron/server/src/manager/app/control.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use anyhow::Context;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::io::{AsyncRead, BufWriter};
-use tokio::process::{Child, Command};
+use tokio::process::{Child, ChildStdin, Command};
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::{mpsc, RwLock};
 use tokio::task::JoinHandle;
@@ -12,6 +12,8 @@ use tracing::warn;
 
 use crate::manager::app::events::{send_event, AppEventType, LineType};
 use crate::manager::app::{Application, ApplicationState};
+
+const INPUT_BUFFER_SIZE: usize = 100;
 
 macro_rules! safely {
     ($broadcast:expr) => {
@@ -23,9 +25,8 @@ macro_rules! safely {
 
 impl Application {
     pub async fn start(&mut self, app_lock: Arc<RwLock<Application>>) -> anyhow::Result<()> {
-        let (sender, receiver) = tokio::sync::mpsc::channel(100);
-        let app_process = self.execute_and_grab_pipes(&app_lock, receiver).await?;
-        let app_task = self.spawn_app_task(app_lock, app_process);
+        let (sender, receiver) = tokio::sync::mpsc::channel(INPUT_BUFFER_SIZE);
+        let app_task = self.spawn_app_tasks(app_lock, receiver).await?;
 
         self.state = ApplicationState::Active {
             app_task,
@@ -35,18 +36,15 @@ impl Application {
         Ok(())
     }
 
-    async fn execute_and_grab_pipes(
+    async fn spawn_app_tasks(
         &self,
-        app_lock: &Arc<RwLock<Application>>,
-        receiver: mpsc::Receiver<String>,
-    ) -> anyhow::Result<Child> {
+        app_lock: Arc<RwLock<Application>>,
+        input_receiver: mpsc::Receiver<String>,
+    ) -> anyhow::Result<JoinHandle<Arc<anyhow::Result<ExitStatus>>>> {
         let mut app_process = self.execute().await?;
+        self.spawn_io_handlers(&mut app_process, &app_lock, input_receiver)?;
 
-        self.spawn_stdin_handler(&mut app_process, receiver)?;
-        self.spawn_stdout_handler(&mut app_process, app_lock.clone())?;
-        self.spawn_stderr_handler(&mut app_process, app_lock.clone())?;
-
-        Ok(app_process)
+        Ok(self.spawn_process_task(app_lock, app_process))
     }
 
     async fn execute(&self) -> anyhow::Result<Child> {
@@ -62,7 +60,90 @@ impl Application {
             .spawn()?)
     }
 
-    fn spawn_app_task(
+    fn spawn_io_handlers(
+        &self,
+        process: &mut Child,
+        app_lock: &Arc<RwLock<Application>>,
+        mut input_receiver: Receiver<String>,
+    ) -> anyhow::Result<()> {
+        let stdio_in = process
+            .stdin
+            .take()
+            .context("new application process lacks stdin")?;
+        let stdio_out = process
+            .stdout
+            .take()
+            .context("new application process lacks stdout")?;
+        let stdio_err = process
+            .stderr
+            .take()
+            .context("new application process lacks stderr")?;
+
+        self.spawn_input_handler(stdio_in, input_receiver);
+        self.spawn_output_handler(stdio_out, app_lock.clone(), |line| LineType::Out(line));
+        self.spawn_output_handler(stdio_err, app_lock.clone(), |line| LineType::Error(line));
+
+        Ok(())
+    }
+
+    fn spawn_input_handler(&self, child_in: ChildStdin, mut input_receiver: Receiver<String>) {
+        tokio::spawn(async move {
+            let mut input_writer = BufWriter::new(child_in);
+
+            loop {
+                let receive_result = input_receiver.recv().await;
+
+                match receive_result {
+                    Some(bytes) => match input_writer.write_all(bytes.as_bytes()).await {
+                        Ok(_) => {}
+                        Err(error) => {
+                            warn!(?error)
+                        }
+                    },
+                    None => return anyhow::Result::<()>::Ok(()),
+                }
+            }
+        });
+    }
+
+    fn spawn_output_handler<T>(
+        &self,
+        child_out: T,
+        app_lock: Arc<RwLock<Application>>,
+        event_provider: fn(String) -> LineType,
+    ) where
+        T: AsyncRead + Unpin + Send + 'static,
+    {
+        tokio::spawn(async move {
+            let mut output_reader = BufReader::new(child_out);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = output_reader.read_line(&mut line).await;
+
+                match bytes_read {
+                    Ok(bytes_read) => {
+                        if bytes_read == 0 {
+                            return anyhow::Result::<()>::Ok(());
+                        }
+
+                        send_event(
+                            &app_lock,
+                            AppEventType::Print {
+                                line: event_provider(line.clone()),
+                            },
+                        )
+                        .await?;
+                    }
+                    Err(error) => {
+                        warn!(?error);
+                    }
+                }
+            }
+        });
+    }
+
+    fn spawn_process_task(
         &self,
         app_lock: Arc<RwLock<Application>>,
         mut app_process: Child,
@@ -93,102 +174,5 @@ impl Application {
 
             execution_result
         })
-    }
-
-    fn spawn_stdin_handler(
-        &self,
-        process: &mut Child,
-        mut receiver: Receiver<String>,
-    ) -> anyhow::Result<()> {
-        let child_in = process
-            .stdin
-            .take()
-            .context("new application process lacks stdin")?;
-
-        tokio::spawn(async move {
-            let mut stdin_writer = BufWriter::new(child_in);
-
-            loop {
-                let receive_result = receiver.recv().await;
-
-                match receive_result {
-                    Some(bytes) => match stdin_writer.write_all(bytes.as_bytes()).await {
-                        Ok(_) => {}
-                        Err(error) => {
-                            warn!(?error)
-                        }
-                    },
-                    None => return anyhow::Result::<()>::Ok(()),
-                }
-            }
-        });
-
-        Ok(())
-    }
-    fn spawn_stdout_handler(
-        &self,
-        process: &mut Child,
-        app_lock: Arc<RwLock<Application>>,
-    ) -> anyhow::Result<()> {
-        let child_out = process
-            .stdout
-            .take()
-            .context("new application process lacks stdout")?;
-
-        self.spawn_output_handler(child_out, app_lock, |line| LineType::Out(line));
-
-        Ok(())
-    }
-
-    fn spawn_stderr_handler(
-        &self,
-        process: &mut Child,
-        app_lock: Arc<RwLock<Application>>,
-    ) -> anyhow::Result<()> {
-        let child_out = process
-            .stderr
-            .take()
-            .context("new application process lacks stdout")?;
-
-        self.spawn_output_handler(child_out, app_lock, |line| LineType::Error(line));
-
-        Ok(())
-    }
-
-    fn spawn_output_handler<T>(
-        &self,
-        child_out: T,
-        app_lock: Arc<RwLock<Application>>,
-        event_provider: fn(String) -> LineType,
-    ) where
-        T: AsyncRead + Unpin + Send + 'static,
-    {
-        tokio::spawn(async move {
-            let mut stdout_reader = BufReader::new(child_out);
-            let mut line = String::new();
-
-            loop {
-                let bytes_read = stdout_reader.read_line(&mut line).await;
-
-                match bytes_read {
-                    Ok(bytes_read) => {
-                        if bytes_read == 0 {
-                            return anyhow::Result::<()>::Ok(());
-                        }
-
-                        send_event(
-                            &app_lock,
-                            AppEventType::Print {
-                                line: event_provider(line.clone()),
-                            },
-                        )
-                        .await?;
-                    }
-                    Err(error) => {
-                        warn!(?error);
-                    }
-                }
-            }
-        });
     }
 }
