88e58fd4129846f8ff13a06f555922a67309bbd1
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2023-10-26 20:11:41 -0500

    Changed the log handler to only hold a file open when the app is running

--- packages/iron/server/src/manager/log_handler.rs
+++ packages/iron/server/src/manager/log_handler.rs
@@ -1,9 +1,10 @@
+use std::path::PathBuf;
 use std::sync::Arc;
 
 use anyhow::Context;
 use async_trait::async_trait;
 use tokio::fs::File;
-use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncWriteExt, BufWriter};
 use tokio::sync::RwLock;
 use tracing::{info, warn};
 
@@ -12,24 +13,60 @@ use crate::manager::app::Application;
 
 #[derive(Debug)]
 pub struct LogHandler {
-    log_file: RwLock<File>,
+    log_file_path: PathBuf,
+    state: RwLock<LogHandlerState>,
+}
+
+#[derive(Debug)]
+struct LogHandlerState {
+    file_writer: Option<BufWriter<File>>,
 }
 
 impl LogHandler {
     pub async fn new(app: &Application) -> anyhow::Result<Self> {
+        let app_directory = &app.config.directory;
+        let log_file_path = app_directory.join("application.log");
+
         Ok(LogHandler {
-            log_file: RwLock::new(create_log_file(app).await?),
+            log_file_path,
+            state: RwLock::new(LogHandlerState { file_writer: None }),
         })
+    }
+
+    async fn open_writer(&self) -> anyhow::Result<()> {
+        let mut state = self.state.write().await;
+
+        match &state.file_writer {
+            None => {
+                state.open_writer(&self.log_file_path).await?;
+
+                Ok(())
+            }
+            Some(_) => Ok(()),
+        }
     }
 }
 
-async fn create_log_file(app: &Application) -> anyhow::Result<File> {
-    let app_directory = &app.config.directory;
-    let log_file_path = app_directory.join("application.log");
-    tokio::fs::create_dir_all(app_directory).await?;
-    let log_file = File::create(log_file_path).await?;
+impl LogHandlerState {
+    async fn open_writer(&mut self, path: &PathBuf) -> anyhow::Result<()> {
+        let log_file = self.create_log_file(path).await?;
+        let log_file_writer = BufWriter::new(log_file);
 
-    Ok(log_file)
+        self.file_writer = Some(log_file_writer);
+
+        Ok(())
+    }
+
+    async fn create_log_file(&self, path: &PathBuf) -> anyhow::Result<File> {
+        tokio::fs::create_dir_all(path).await?;
+        let log_file = File::create(path).await?;
+
+        Ok(log_file)
+    }
+
+    fn close_writer(&mut self) {
+        self.file_writer = None;
+    }
 }
 
 #[async_trait]
@@ -37,16 +74,21 @@ impl AppEventHandler for LogHandler {
     async fn handle(&self, event: Arc<AppEvent>) -> anyhow::Result<()> {
         match &event.event_type {
             AppEventType::Start { .. } => {
+                self.open_writer().await?;
                 info!("app started")
             }
-            AppEventType::End { result } => match &**result {
-                Ok(status) => {
-                    info!(?status)
+            AppEventType::End { result } => {
+                match &**result {
+                    Ok(status) => {
+                        info!(?status);
+                    }
+                    Err(error) => {
+                        warn!(?error);
+                    }
                 }
-                Err(error) => {
-                    info!(?error)
-                }
-            },
+
+                self.state.write().await.close_writer();
+            }
             AppEventType::Print { line } => {
                 let text = match line {
                     LineType::Out(out_line) => {
@@ -59,12 +101,19 @@ impl AppEventHandler for LogHandler {
                     }
                 };
 
-                self.log_file
-                    .write()
-                    .await
-                    .write_all(text.as_bytes())
-                    .await
-                    .context("failed to write to log file")?;
+                let mut state = self.state.write().await;
+
+                match &mut state.file_writer {
+                    None => {
+                        state.open_writer(&self.log_file_path).await?;
+                    }
+                    Some(writer) => {
+                        writer
+                            .write_all(text.as_bytes())
+                            .await
+                            .context("failed to write to log file")?;
+                    }
+                };
             }
         }
 
